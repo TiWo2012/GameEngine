e3b3908a13e20c40d4a0b08bfa10953a195187a3
TiWo2012 <timo.wolf@gmx.ch>
2025-08-27 20:16:18 +0200

    added comments to make it easyser to understand

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
+/// \file
+/// \brief The main entry point of the application
+///
+/// This is the main entry point of the application. It initializes GLFW,
+/// creates a window, initializes GLAD, creates a vertex buffer object,
+/// compiles and links a shader program, and enters the main loop.
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+/// \brief The vertex shader source code
 const char* vertexShaderSource = R"(
 #version 330 core
 layout(location = 0) in vec3 aPos;
@@ -15,6 +23,7 @@ void main() {
 }
 )";
 
+/// \brief The fragment shader source code
 const char* fragmentShaderSource = R"(
 #version 330 core
 in vec3 vertexColor;
@@ -26,6 +35,10 @@ void main() {
 }
 )";
 
+/// \brief Compile a shader
+/// \param type The type of shader to compile (GL_VERTEX_SHADER or GL_FRAGMENT_SHADER)
+/// \param source The source code of the shader
+/// \return The handle of the compiled shader
 unsigned int compileShader(unsigned int type, const char* source) {
     unsigned int shader = glCreateShader(type);
     glShaderSource(shader, 1, &source, nullptr);
@@ -42,6 +55,8 @@ unsigned int compileShader(unsigned int type, const char* source) {
     return shader;
 }
 
+/// \brief Create and link a shader program
+/// \return The handle of the linked shader program
 unsigned int createShaderProgram() {
     unsigned int vertexShader = compileShader(GL_VERTEX_SHADER, vertexShaderSource);
     unsigned int fragmentShader = compileShader(GL_FRAGMENT_SHADER, fragmentShaderSource);
@@ -66,17 +81,20 @@ unsigned int createShaderProgram() {
     return shaderProgram;
 }
 
+/// \brief The main entry point of the application
 int main() {
+    // Initialize GLFW
     if (!glfwInit()) {
         std::cerr << "Failed to initialize GLFW\n";
         return -1;
     }
 
-    // OpenGL 3.3 core profile
+    // Set up OpenGL 3.3 core profile
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+    // Create a windowed mode window and its OpenGL context
     GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Window", nullptr, nullptr);
     if (!window) {
         std::cerr << "Failed to create GLFW window\n";
@@ -84,13 +102,28 @@ int main() {
         return -1;
     }
 
+    // Make the window's context current
     glfwMakeContextCurrent(window);
 
+    // Initialize GLAD, which loads the OpenGL functions
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         std::cerr << "Failed to initialize GLAD\n";
         return -1;
     }
 
+    // Create buffer and vertex array objects
+    unsigned int VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    // Bind vertex array object and vertex buffer object
+    // The vertex array object is used to store the vertex attribute data
+    glBindVertexArray(VAO);
+
+    // The vertex buffer object is used to store the vertex data
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    // Define vertices and colors of the triangle
     float vertices[] = {
         // positions        // colors
          0.0f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // top vertex, red
@@ -98,52 +131,54 @@ int main() {
          0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // bottom right, blue
     };
 
-    unsigned int VBO, VAO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-
-    // Bind VAO first
-    glBindVertexArray(VAO);
-
-    // Bind VBO, buffer vertex data
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    // Copy the vertex data into the vertex buffer object
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    // Configure vertex attribute (position)
-    // position attribute (location = 0)
+    // Configure vertex attributes
+    // The position attribute is at location 0
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
-    // color attribute (location = 1)
+    // The color attribute is at location 1
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
-    // Unbind (optional)
+    // Unbind VAO and VBO
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
+    // Create and link the shader program
     unsigned int shaderProgram = createShaderProgram();
 
+    // Main loop
     while (!glfwWindowShouldClose(window)) {
+        // Clear the screen with a dark blue color
         glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // Use shader program
+        // Use the shader program
         glUseProgram(shaderProgram);
+
         // Bind VAO
         glBindVertexArray(VAO);
+
         // Draw the triangle
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
+        // Swap the front and back buffers
         glfwSwapBuffers(window);
+        // Poll events
         glfwPollEvents();
     }
 
+    // Clean up
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
     glDeleteProgram(shaderProgram);
 
+    // Terminate GLFW
     glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
 }
+
